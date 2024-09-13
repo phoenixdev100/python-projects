8ae602311c064c668afa65ae35616e65358dcd57
Deepak Chaudhary <deepakkumar312005@gmail.com>
2024-09-13 09:16:13 +0530

    Added emoji.py

---
+++ emoji.py
@@ -0,0 +1,7 @@
+import emoji
+
+print(emoji.emojize("I love reading books:books:"))
+print(emoji.emojize("Some people have a very sensitive heart:red_heart:, please be kind with them.:hibiscus:"))
+
+# emoji list
+# https://carpedm20.github.io/emoji/
